ab8c7a51fa3f9c277c92d8824d0da030295f8b73
Tess Huelskamp <tess.huelskamp@gmail.com>
2017-02-09 17:37:42 -0500

    showcase simple UI elements and live variable

--- source/gplix.cc
+++ source/gplix.cc
@@ -7,8 +7,41 @@
 namespace UI = emp::web;
 
 UI::Document doc("emp_base");
+int x = 5; 
+
+void incX(){ x++; doc.Redraw();} 
+
 
 int main()
 {
+
   doc << "<h1>Hello, world!</h1>";
+  doc << x << " <br>"; 
+  doc << UI::Live(x) << "<br>" ; 
+
+  UI::Table table(12,12); 
+
+
+
+  for (int i=0; i<12; i++){
+      table.GetCell(i,i) <<"hi";
+  }
+
+  UI::Button button(incX, "inc x"); 
+  doc << button; 
+
+  table.GetCell(3,5) << "hello world again"; 
+  table.GetCell(9,6) << "hello world again"; 
+
+  table.GetTable(); 
+  table.SetCSS("border","1px solid green"); 
+
+
+  doc << table; 
+
+  UI::Image train("train.png"); 
+
+  doc << train; 
+
+
 }
